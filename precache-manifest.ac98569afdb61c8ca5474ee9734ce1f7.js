ad3370b6780446e9f629ba196752aa5331dbc706
Jisung Yoo <jyoo@ezcaretech.com>
2020-02-18 20:13:01 +0900

    Updates

---
+++ precache-manifest.ac98569afdb61c8ca5474ee9734ce1f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffdecea1860ed2a73b62fd6b82a25eb6",
+    "url": "/ez-dead-or-alive/index.html"
+  },
+  {
+    "revision": "4e55d07403fb70506bc5",
+    "url": "/ez-dead-or-alive/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5fecdf9ff209b90c6d90",
+    "url": "/ez-dead-or-alive/static/js/2.9699aabf.chunk.js"
+  },
+  {
+    "revision": "0553b7b7af8aecd28f9c4b58cf580311",
+    "url": "/ez-dead-or-alive/static/js/2.9699aabf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e55d07403fb70506bc5",
+    "url": "/ez-dead-or-alive/static/js/main.df3b8c6a.chunk.js"
+  },
+  {
+    "revision": "b67409691e32c8c20b2a",
+    "url": "/ez-dead-or-alive/static/js/runtime-main.76899ee5.js"
+  }
+]);
